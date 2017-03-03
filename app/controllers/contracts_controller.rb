7b26c12f2845b08bce7286ca0c99e13371bb3316
Cy Weisman <cyweisman@gmail.com>
2017-03-03 16:09:22 -0600

    made class variables in draft

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -8,13 +8,18 @@ class ContractsController < ApplicationController
     @player = Player.find(@contract.player_id)
     @player.team_id = params[:team_id]
     @player.save
-    @draft = Draft.find(params)
+    @draft = Draft.find(params[:draft_id])
     @draft.next_pick
-    if @draft.get_round == 7
+    #DEBUG NEXT STEP - WHY ITS CONTiNUIgn to go thru rounds
+    if @draft.get_round == @draft.number_of_rounds + 1
       redirect_to teams_path
     else
       redirect_to draft_path(@draft.id)
     end
   end
+
+  # def draft_params
+  #   params.require(:contract).permit(:team_id, :player_id, :salary, :draft)
+  # end
 
 end

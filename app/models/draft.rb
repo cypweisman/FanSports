7c0bb862b3d32dd2a60b30e78d9a1c28fe2b9bb3
Cy Weisman <cyweisman@gmail.com>
2017-03-27 13:09:03 -0500

    add attr_accesor

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,11 +1,19 @@
 class Draft < ApplicationRecord
+  attr_accessor :round, :shuffled_teams, :drafting_counter
 
-  @@shuffled_teams = []
-  @@round = 1
-  @@drafting_counter = 0
+  def initialize(draft_params)
+    super(draft_params)
+    round = 1
+    drafting_counter = 0
+    shuffled_teams = []
+  end
+
+  # @@shuffled_teams = []
+  # @@round = 1
+  # @@drafting_counter = 0
 
   def get_drafting_counter
-    @@drafting_counter
+    drafting_counter
   end
 
   # def set_drafting_counter(number)
@@ -13,31 +21,31 @@ class Draft < ApplicationRecord
   # end
 
   def get_round
-    @@round
+    round
   end
 
   def set_shuffled_teams
-    @@round = 1
-    @@drafting_counter = 0
-    @@shuffled_teams = Team.all.shuffle
+    round = 1
+    drafting_counter = 0
+    shuffled_teams = Team.all.shuffle
   end
 
   def get_shuffled_teams
-    if @@shuffled_teams == []
+    if shuffled_teams == []
       #was not working when == nil
       set_shuffled_teams
     end
-    @@shuffled_teams
+    shuffled_teams
   end
 
   def next_pick
-    if @@drafting_counter != @@shuffled_teams.length - 1
-      @@drafting_counter +=1
+    if drafting_counter != shuffled_teams.length - 1
+      drafting_counter +=1
     else
-      @@drafting_counter = 0
-      @@shuffled_teams.reverse!
-      @@round +=1
-      if @@round == self.number_of_rounds + 1
+      drafting_counter = 0
+      shuffled_teams.reverse!
+      round +=1
+      if round == self.number_of_rounds + 1
         self.open = false
         self.save
       end

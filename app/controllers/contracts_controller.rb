61d405ca5fa9bcb1d8b525f956801703f50459e7
Cy Weisman <cyweisman@gmail.com>
2017-03-28 12:43:15 -0500

    add callback for contract

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -3,12 +3,8 @@ include DraftHelper
 class ContractsController < ApplicationController
 
   def create
-    p params
+    #p params
     @contract = Contract.create(team_id: params[:team_id], player_id: params[:player_id], salary: params[:salary], active: true)
-    #do a after_create callback on the contract model
-    @player = Player.find(@contract.player_id)
-    @player.team_id = params[:team_id]
-    @player.save
     @draft = Draft.find(params[:draft_id])
     next_pick(@draft)
     redirect_to draft_path(@draft.id)
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,5 +2,12 @@ class Contract < ApplicationRecord
   belongs_to :team
   belongs_to :player
 
+  after_create :update_player_contract
+
+  def update_player_contract
+    player = Player.find(self.player_id)
+    player.team_id = self.team_id
+    player.save
+  end
 
 end
